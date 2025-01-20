a79e18153e8ef233b92b287d44e854c2f10c8658
Kevin Lammers <klammers@daytonfreight.com>
2025-01-20 11:51:34 -0500

    Changes to be committed: 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts 	new file:   src/app/catalog/catalog.component.css 	new file:   src/app/catalog/catalog.component.html 	new file:   src/app/catalog/catalog.component.spec.ts     new file:   src/app/catalog/catalog.component.ts     modified:   src/app/home/home.component.css 	modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts 	new file:   src/assets/images/hero-banner.png 	new file:   src/assets/images/logo.png 	new file:   src/assets/images/profile.png 	new file:   src/assets/images/robot-apocalypse.png 	new file:   src/assets/images/robot-parts/arm-articulated-claw.png 	new file:   src/assets/images/robot-parts/arm-dual-claw.png 	new file:   src/assets/images/robot-parts/arm-grabber.png 	new file:   src/assets/images/robot-parts/arm-propeller.png 	new file:   src/assets/images/robot-parts/arm-stubby-claw.png 	new file:   src/assets/images/robot-parts/base-double-wheel.png 	new file:   src/assets/images/robot-parts/base-rocket.png 	new file:   src/assets/images/robot-parts/base-single-wheel.png 	new file:   src/assets/images/robot-parts/base-spring.png 	new file:   src/assets/images/robot-parts/base-triple-wheel.png 	new file:   src/assets/images/robot-parts/example_robot.png 	new file:   src/assets/images/robot-parts/head-big-eye.png 	new file:   src/assets/images/robot-parts/head-friendly.png 	new file:   src/assets/images/robot-parts/head-shredder.png 	new file:   src/assets/images/robot-parts/head-single-eye.png 	new file:   src/assets/images/robot-parts/head-surveillance.png 	new file:   src/assets/images/robot-parts/robot.png 	new file:   src/assets/images/robot-parts/torso-flexible-gauged.png 	new file:   src/assets/images/robot-parts/torso-gauged.png 	new file:   src/assets/images/robot-parts/torso-pouch.png 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CatalogComponent } from "./catalog/catalog.component";
+import { CartComponent } from "../../_course-resources/cart/cart.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  imports: [CatalogComponent, CartComponent]
 })
 export class AppComponent {
 }
